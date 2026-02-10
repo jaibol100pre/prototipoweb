45a0c9596ca3d95435ceabace592a7d93e01c2d2
jaibol100pre <jaibolsantaella@gmail.com>
2026-02-10 11:45:35 -0400

    agregando slider

--- demo4/js/main.js
+++ demo4/js/main.js
@@ -33,3 +33,39 @@ function openQuickView(brand, title, price, imgUrl) {
     document.getElementById('qv-price').innerText = price;
     document.getElementById('qv-image').src = imgUrl;
 }
+
+
+
+/** Carousel */
+
+$(function() {
+
+	if ( $('.owl-2').length > 0 ) {
+        $('.owl-2').owlCarousel({
+            center: false,
+            items: 1,
+            loop: true,
+            stagePadding: 0,
+            margin: 20,
+            smartSpeed: 1000,
+            autoplay: true,
+            nav: true,
+            dots: true,
+            pauseOnHover: false,
+            responsive:{
+                600:{
+                    margin: 20,
+                    nav: true,
+                  items: 2
+                },
+                1000:{
+                    margin: 20,
+                    stagePadding: 0,
+                    nav: true,
+                  items: 3
+                }
+            }
+        });            
+    }
+
+})
